7c6eb6f2fad532bd5032803ee35e837a0eda46aa
Rushikesh Madhukar Sutar <155623277+rushi2207@users.noreply.github.com>
2025-09-26 16:18:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Web Page 🚀</h1>
+      <p>This page is deployed on AWS EC2 using Terraform!</p>
+    </div>
+  );
+}
+
+export default App;
