3a4bc160c22ce5959a05a3f5d5dc604b6449ca09
Rushikesh Madhukar Sutar <155623277+rushi2207@users.noreply.github.com>
2025-09-29 15:14:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to My Web Page 🚀 this is new version </h1>
+      <h1>Welcome to My Web Page 🚀 this is new version 2 </h1>
       <p>This page is deployed on AWS EC2 using Terraform!</p>
     </div>
   );
